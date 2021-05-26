774e0517d76f28faae46d276e6e7267cd29d2cee
Petr Korobeinikov <pkorobeinikov@gmail.com>
2021-05-26 11:52:18 +0300

    Go-way if-else stmt.

--- internal/lookup.go
+++ internal/lookup.go
@@ -5,7 +5,7 @@ import "os"
 func LookupEnvOrDefault(key, defaultValue string) (value string, found bool) {
 	if value, found := os.LookupEnv(key); found {
 		return value, found
-	} else {
-		return defaultValue, false
 	}
+
+	return defaultValue, false
 }
